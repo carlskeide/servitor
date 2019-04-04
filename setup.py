dcc4c6a36fb737e96d39d971ca08af224b44ad76
Carl Skeide <carl@skeide.se>
2019-04-04 11:19:12 +0200

    Make package installable

---
+++ setup.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='Servitor',
+    version='1.0.4',
+
+    description='Tiny webhook for pushing images to docker swarm services',
+    url='https://github.com/carlskeide/servitor/',
+    author='Carl Skeide',
+
+    packages=find_packages(),
+
+    install_requires=[
+        "flask",
+        "flask-restful",
+        "requests",
+        "PyYAML",
+        "docker"
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ],
+    }
+)
