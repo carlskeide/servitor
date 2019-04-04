6861c85de8e222ae12ec24d9667a6989be7ba2c9
Carl Skeide <carl@skeide.se>
2019-04-04 13:00:27 +0200

    Test all supported HTTP verbs

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,21 +12,39 @@ class TestAPI(TestCase):
         res = self.client.get('/')
         self.assertEqual(res.status_code, 404)
 
+    @patch("servitor.Service.put")
     @patch("servitor.Service.get")
-    def test_service(self, mock_service):
-        mock_service.return_value = {}
+    def test_service(self, mock_get, mock_put):
+        mock_get.return_value = {}
         res = self.client.get('/service/some-swarm/some-service',
                               query_string={"token": TEST_TOKEN})
 
-        mock_service.assert_called_with(
+        mock_get.assert_called_with(
             env="some-swarm", name="some-service")
         self.assertEqual(res.status_code, 200)
 
+        mock_put.return_value = {}
+        res = self.client.put('/service/some-swarm/some-service',
+                              query_string={"token": TEST_TOKEN})
+
+        mock_put.assert_called_with(
+            env="some-swarm", name="some-service")
+        self.assertEqual(res.status_code, 200)
+
+    @patch("servitor.Stack.put")
     @patch("servitor.Stack.get")
-    def test_stack(self, mock_stack):
-        mock_stack.return_value = {}
+    def test_stack(self, mock_get, mock_put):
+        mock_get.return_value = {}
+
         res = self.client.get('/stack/some-swarm/some-stack',
                               query_string={"token": TEST_TOKEN})
 
-        mock_stack.assert_called_with(env="some-swarm", name="some-stack")
+        mock_get.assert_called_with(env="some-swarm", name="some-stack")
+        self.assertEqual(res.status_code, 200)
+
+        mock_put.return_value = {}
+        res = self.client.put('/stack/some-swarm/some-stack',
+                              query_string={"token": TEST_TOKEN})
+
+        mock_put.assert_called_with(env="some-swarm", name="some-stack")
         self.assertEqual(res.status_code, 200)
